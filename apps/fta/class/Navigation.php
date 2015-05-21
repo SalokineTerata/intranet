ffe609fbb6a28452723942356803f5e23c2c15eb
tp4300001 <tp4300001@pwn043126.avignon.grpldc.com>
2015-05-21 17:25:35 +0200

    [V] Mise en fonction de la validation d'un chapitre. Avec sauvegarde de l'état de validation dans la base de donnéee [V] Cohérence de la validation des chapitres / en phase de test : fonction notification_suivie_projet  modif [V] Creation de log en cours : 	historique [V] /	 transactions /Processus suivant  [V] / 		Valaidation fta [] / 		Modification/correction [] / [V]Création d'un workflow exemple: Création des champs de chaque chapitre  [V]/		 Mise en fonction de la barre de navigation [V] [] Mise en fonction des workflow []Optimisation de la page modification_fiche_poste. [V] Mise en fonction de la gestion de validation d'un chapitre. []Chapitre Emballage

--- apps/fta/class/Navigation.php
+++ apps/fta/class/Navigation.php
@@ -7,7 +7,6 @@
  */
 class Navigation {
 
-
     protected static $id_fta;
     protected static $comeback;
     protected static $comeback_url;
@@ -15,8 +14,6 @@ class Navigation {
     protected static $html_navigation_core;
     //protected static $id_fta_chapitre;
     protected static $id_fta_chapitre_encours;
-
-
     protected static $synthese_action;
     protected static $id_fta_processus;
 
@@ -115,7 +112,7 @@ class Navigation {
 
 
 
-        
+
         //Si une action est donnée, alors construction du menu des chapitres
         if (self::$synthese_action) {
             //Etat d'avancement de la FTA et Recherche des processus validés (et donc en lecture-seule)
@@ -142,12 +139,22 @@ class Navigation {
             }//Fin du balayage
             //Recherche des processus en cours
             //Balayage des cycles des processus (en excluant les processus déjà validés)
+
+            /*
+             * Sur ce niveau un processus doit selectionner le premier chapitre du groupe / la notion de rôle doit e^tre implement auparavant
+             * car cela gènère des erreurs ou faute dans ma requete sql car 
+             * quand je valide un chap d'un processus cela débloque tous les chapitre du meme role 
+             * alors quil manque d'autre chapitre a valider pour valider le processus
+             */
             $req = "SELECT DISTINCT " . FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT
                     . " FROM " . FtaProcessusCycleModel::TABLENAME
                     . "," . FtaProcessusModel::TABLENAME
                     . "," . IntranetActionsModel::TABLENAME
                     . "," . IntranetDroitsAccesModel::TABLENAME
                     . "," . IntranetModulesModel::TABLENAME
+                    //. "," . FtaModel::TABLENAME
+                    //. "," . FtaSuiviProjetModel::TABLENAME
+                    //. "," . FtaChapitreModel::TABLENAME
                     . " WHERE 1 AND ( 1 "
             ;
 
@@ -164,73 +171,82 @@ class Navigation {
                     . "=" . IntranetDroitsAccesModel::TABLENAME . "." . IntranetDroitsAccesModel::FIELDNAME_ID_INTRANET_ACTIONS  //Jointure
                     . " AND " . IntranetDroitsAccesModel::TABLENAME . "." . IntranetDroitsAccesModel::FIELDNAME_ID_INTRANET_MODULES
                     . "=" . IntranetModulesModel::TABLENAME . "." . IntranetModulesModel::KEYNAME  //Jointure
+                   // . " AND " . FtaModel::TABLENAME . "." . FtaModel::KEYNAME
+                    //. "=" . FtaSuiviProjetModel::TABLENAME . "." . FtaSuiviProjetModel::FIELDNAME_ID_FTA  //Jointure
+                    //. " AND " . FtaChapitreModel::TABLENAME . "." . FtaChapitreModel::KEYNAME
+                    //. "=" . FtaSuiviProjetModel::TABLENAME . "." . FtaSuiviProjetModel::FIELDNAME_ID_FTA_CHAPITRE  //Jointure
+                   // . " AND " . FtaChapitreModel::TABLENAME . "." . FtaChapitreModel::FIELDNAME_ID_PROCESSUS
+                   // . "=" . FtaProcessusCycleModel::TABLENAME . "." . FtaProcessusCycleModel::FIELDNAME_PROCESSUS_INIT  //Jointure
+                   // . " AND " . FtaSuiviProjetModel::FIELDNAME_SIGNATURE_VALIDATION_SUIVI_PROJET . "<>0" //chapitre validé
                     . " AND " . IntranetDroitsAccesModel::FIELDNAME_ID_USER . "=" . $id_user //Utilisateur actuellement connecté
                     . " AND " . IntranetModulesModel::FIELDNAME_NOM_INTRANET_MODULES . "='" . FtaModel::TABLENAME
                     . "' AND " . IntranetDroitsAccesModel::FIELDNAME_NIVEAU_INTRANET_DROITS_ACCES . "=1"  //L'utilisateur est propriétaire
-                   // . " AND " . FtaProcessusCycleModel::FIELDNAME_FTA_ETAT . "='" . $modelFta->getModelFtaEtat()->getDataField(FtaEtatModel::FIELDNAME_ABREVIATION)->getFieldValue() . "' "
-                   // . " AND " . FtaModel::FIELDNAME_CATEGORIE_FTA . "= '" . $modelFta->getDataField(FtaModel::FIELDNAME_CATEGORIE_FTA)->getFieldValue(). "' "
+            // . " AND " . FtaProcessusCycleModel::FIELDNAME_FTA_ETAT . "='" . $modelFta->getModelFtaEtat()->getDataField(FtaEtatModel::FIELDNAME_ABREVIATION)->getFieldValue() . "' "
+            // . " AND " . FtaModel::FIELDNAME_CATEGORIE_FTA . "= '" . $modelFta->getDataField(FtaModel::FIELDNAME_CATEGORIE_FTA)->getFieldValue(). "' "
             ;
 
             //Finalisation de la requête
             $req .="";
 
             $result = DatabaseOperation::convertSqlQueryWithAutomaticKeyToArray($req);
-            foreach ($result as $rows) {
-                //Pour chaque processus, on vérifie que tous ces précédents sont validés
-                $req = "SELECT * FROM " . FtaProcessusCycleModel::TABLENAME
-                        . " WHERE " . FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT
-                        . "=" . $rows[FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT]
-                        . " AND ( 1 "
-                ;
+            if ($result) {
+                foreach ($result as $rows) {
+                    //Pour chaque processus, on vérifie que tous ces précédents sont validés
+                    $req = "SELECT " . FtaProcessusCycleModel::KEYNAME . " FROM " . FtaProcessusCycleModel::TABLENAME
+                            . " WHERE " . FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT
+                            . "=" . $rows[FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT]
+                            . " AND ( 1 "
+                    ;
 
-                //Ajout de la restriction des processus validé
-                $req .=self::AddValidProcess($t_processus_visible);
+                    //Ajout de la restriction des processus validé
+                    $req .=self::AddValidProcess($t_processus_visible);
 
-                //Recherche dans le cycle correspondant à l'état en cours de la fiche
-                $req_etat = DatabaseOperation::convertSqlQueryWithAutomaticKeyToArray(
-                                "SELECT " . FtaEtatModel::TABLENAME . "." . FtaEtatModel::FIELDNAME_ABREVIATION
-                                . " FROM " . FtaEtatModel::TABLENAME . "," . FtaModel::TABLENAME
-                                . " WHERE " . FtaEtatModel::TABLENAME . "." . FtaEtatModel::KEYNAME
-                                . "=" . FtaModel::TABLENAME . "." . FtaModel::FIELDNAME_ID_FTA_ETAT
-                                . " AND " . FtaModel::TABLENAME . "." . FtaModel::KEYNAME
-                                . "= '" . self::$id_fta . "'"
-                );
-
-                foreach ($req_etat as $rowsEtat) {
-                    $abreviation_fta_etat = $rowsEtat[FtaEtatModel::FIELDNAME_ABREVIATION];
-                }
-
-                $req .= "AND " . FtaProcessusCycleModel::FIELDNAME_FTA_ETAT
-                        . "='" . $abreviation_fta_etat . "' ";
-
-                //Filtrage par catégorie
-                //Finalisation de la requête
-                $req .=")";
-
-                //Si la requête est vide, c'est que tous les processus précédents sont validés
-                //Il est donc un Processus en cours
-                $req_temp = DatabaseOperation::query($req);
-
-                if (DatabaseOperation::getSqlNumRows($req_temp)) {
-                    //Ce processus en cours, est-il du type repartie ou centralisé ?
-                    $reqType = DatabaseOperation::convertSqlQueryWithAutomaticKeyToArray(
-                                    "SELECT " . FtaProcessusModel::FIELDNAME_MULTISITE_FTA_PROCESSUS
-                                    . " FROM " . FtaProcessusModel::TABLENAME
-                                    . " WHERE " . FtaProcessusModel::KEYNAME
-                                    . "=" . $rows[FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT]
+                    //Recherche dans le cycle correspondant à l'état en cours de la fiche
+                    $req_etat = DatabaseOperation::convertSqlQueryWithAutomaticKeyToArray(
+                                    "SELECT " . FtaEtatModel::TABLENAME . "." . FtaEtatModel::FIELDNAME_ABREVIATION
+                                    . " FROM " . FtaEtatModel::TABLENAME . "," . FtaModel::TABLENAME
+                                    . " WHERE " . FtaEtatModel::TABLENAME . "." . FtaEtatModel::KEYNAME
+                                    . "=" . FtaModel::TABLENAME . "." . FtaModel::FIELDNAME_ID_FTA_ETAT
+                                    . " AND " . FtaModel::TABLENAME . "." . FtaModel::KEYNAME
+                                    . "= '" . self::$id_fta . "'"
                     );
-                    foreach ($reqType as $rowsType) {
-                        $multisite_fta_processus = $rowsType[FtaProcessusModel::FIELDNAME_MULTISITE_FTA_PROCESSUS];
+
+                    foreach ($req_etat as $rowsEtat) {
+                        $abreviation_fta_etat = $rowsEtat[FtaEtatModel::FIELDNAME_ABREVIATION];
                     }
-                    if ($multisite_fta_processus) {
-                        //Oui, il s'agit d'un Processus répartie sur les sites d'assemblage
-                        $t_processus_encours = self::CheckMultiSite($rows, $t_processus_encours);
-                    } else {
-                        //Enregistrement du processus en tant que processus en cours
-                        $t_processus_encours[] = $rows[FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT];
+
+                    $req .= "AND " . FtaProcessusCycleModel::FIELDNAME_FTA_ETAT
+                            . "='" . $abreviation_fta_etat . "' ";
+
+                    //Filtrage par catégorie
+                    //Finalisation de la requête
+                    $req .=")";
+
+                    //Si la requête est vide, c'est que tous les processus précédents sont validés
+                    //Il est donc un Processus en cours
+                    $req_temp = DatabaseOperation::query($req);
+
+                    if (DatabaseOperation::getSqlNumRows($req_temp)) {
+                        //Ce processus en cours, est-il du type repartie ou centralisé ?
+                        $reqType = DatabaseOperation::convertSqlQueryWithAutomaticKeyToArray(
+                                        "SELECT " . FtaProcessusModel::FIELDNAME_MULTISITE_FTA_PROCESSUS
+                                        . " FROM " . FtaProcessusModel::TABLENAME
+                                        . " WHERE " . FtaProcessusModel::KEYNAME
+                                        . "=" . $rows[FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT]
+                        );
+                        foreach ($reqType as $rowsType) {
+                            $multisite_fta_processus = $rowsType[FtaProcessusModel::FIELDNAME_MULTISITE_FTA_PROCESSUS];
+                        }
+                        if ($multisite_fta_processus) {
+                            //Oui, il s'agit d'un Processus répartie sur les sites d'assemblage
+                            $t_processus_encours = self::CheckMultiSite($rows, $t_processus_encours);
+                        } else {
+                            //Enregistrement du processus en tant que processus en cours
+                            $t_processus_encours[] = $rows[FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT];
+                        }
                     }
-                }
-            }//Fin du balayage des processus non-validés
+                }//Fin du balayage des processus non-validés
+            }
             //Recherche des processus Publics
             //Création de la liste des processus dans la barre de navigation          
             $t_liste_processus = array_merge($t_processus_encours, $t_processus_visible);
@@ -245,6 +261,7 @@ class Navigation {
                             . " WHERE " . FtaProcessusCycleModel::TABLENAME
                             . "." . FtaProcessusCycleModel::FIELDNAME_PROCESSUS_NEXT . " IS NULL"
             );
+
             foreach ($resultAddProces as $rowsAddProcess) {
                 $t_liste_processus[] = $rowsAddProcess[FtaProcessusModel::KEYNAME];
             }
@@ -259,9 +276,7 @@ class Navigation {
     protected static function RecupChapitre($paramT_Liste_Processus) {
         $page_default = "modification_fiche";
         if ($paramT_Liste_Processus) {
-            /*
-             * @todo AMeliorer la req pas *
-             */
+
             $reqRecup = "SELECT " . FtaChapitreModel::TABLENAME . "." . FtaChapitreModel::KEYNAME
                     . ", " . FtaChapitreModel::TABLENAME . "." . FtaChapitreModel::FIELDNAME_NOM_USUEL_CHAPITRE
                     . ", " . FtaProcessusModel::TABLENAME . "." . FtaProcessusModel::KEYNAME
@@ -275,10 +290,10 @@ class Navigation {
                 $reqRecup .= " OR " . FtaProcessusModel::TABLENAME . "." . FtaProcessusModel::KEYNAME . "=" . $value . " ";
             }
             $reqRecup .=" ) ORDER BY " . FtaChapitreModel::TABLENAME . "." . FtaChapitreModel::KEYNAME;
-            $resultRecup = DatabaseOperation::convertSqlQueryWithAutomaticKeyToArray($reqRecup);
+            $arrayRecup = DatabaseOperation::convertSqlQueryWithAutomaticKeyToArray($reqRecup);
 
             //Balyage des chapitres trouvés
-            foreach ($resultRecup as $rowsRecup) {
+            foreach ($arrayRecup as $rowsRecup) {
                 $id_fta_chapitre = $rowsRecup[FtaChapitreModel::KEYNAME];
                 $nom_usuel_fta_chapitre = $rowsRecup[FtaChapitreModel::FIELDNAME_NOM_USUEL_CHAPITRE];
 

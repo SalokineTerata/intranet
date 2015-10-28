360d5abc52de767dcc2959893da20408f5082cf5
Salokine Terata <salokine@gmail.com>
2015-10-28 12:20:18 +0100

    * Scripts d'importation

--- scripts/importDB-V2toV3.sh
+++ scripts/importDB-V2toV3.sh
@@ -8,11 +8,14 @@
 
 # VARIABLES
 # ---------
+
+ENV_DEST="$1"
+
 DB_NAME="agis"
 DB_USER_PRODV2="lecture_seule"
 DB_PASSWD_PRODV2="agis"
 
-DB_NAME_V3="intranet_v2_0_prod"
+DB_NAME_V3="intranet_v2_0_prod_$ENV_DEST"
 DB_USER_PRODV3="mysqladm"
 DB_PASSWD_PRODV3="agis"
 
@@ -25,28 +28,28 @@ CHARSET_DEST="UTF-8"
 EXPORT_FILE="export$DB_NAME"
 EXPORT_FILE_CONV="$EXPORT_FILE-$CHARSET_DEST"
 
-SERVER_DEST="cod-intranet.agis.fr"
+SERVER_DEST="$ENV_DEST-intranet.agis.fr"
 DIR_DEST="/tmp"
 
 # EXPORTATION
 # -----------
 
 echo "* Récupération du dump MySQL"
-#mysqldump --quote-names --user=root -p $DB_NAME > $ARCH_DIR/$EXPORT_FILE.sql
-ssh -n $SRV_PRODV2 -- mysqldump -a --add-drop-table -v --quote-names --user=$DB_USER_PRODV2 --password=$DB_PASSWD_PRODV2 $DB_NAME > $DIR_DEST/$EXPORT_FILE.sql
+#ssh -n $SRV_PRODV2 -- mysqldump -a --add-drop-table -v --quote-names --user=$DB_USER_PRODV2 --password=$DB_PASSWD_PRODV2 $DB_NAME > $DIR_DEST/$EXPORT_FILE.sql
 
 echo "* Archivage du Dump brut"
-rsync -Phavz $DIR_DEST/$EXPORT_FILE.sql $SRV_ARCH:$ARCH_DIR/$EXPORT_FILE.sql
+#rsync -Phavz $DIR_DEST/$EXPORT_FILE.sql $SRV_ARCH:$ARCH_DIR/$EXPORT_FILE.sql
 
 echo "* Conversion charset"
-iconv -f $CHARSET_ORIG -t $CHARSET_DEST $DIR_DEST/$EXPORT_FILE.sql -o $DIR_DEST/$EXPORT_FILE_CONV.sql
+#iconv -f $CHARSET_ORIG -t $CHARSET_DEST $DIR_DEST/$EXPORT_FILE.sql -o $DIR_DEST/$EXPORT_FILE_CONV.sql
 
 echo "* Correction Engine"
-sed -i -e "s/TYPE=MyISAM/ENGINE=MyISAM/g" $DIR_DEST/$EXPORT_FILE_CONV.sql
+#sed -i -e "s/TYPE=MyISAM/ENGINE=MyISAM/g" $DIR_DEST/$EXPORT_FILE_CONV.sql
 
 echo "* Correction Timestamp"
-sed -i -e "s/timestamp(14)/timestamp/g" $DIR_DEST/$EXPORT_FILE_CONV.sql
+#sed -i -e "s/timestamp(14)/timestamp/g" $DIR_DEST/$EXPORT_FILE_CONV.sql
 
 echo "* Importation des données dans le serveur de base de données local"
-mysql --user=$DB_USER_PRODV3 --password=$DB_PASSWD_PRODV3 $DB_NAME_V3 < $DIR_DEST/$EXPORT_FILE_CONV.sql
+#mysql --user=$DB_USER_PRODV3 --password=$DB_PASSWD_PRODV3 $DB_NAME_V3 < $DIR_DEST/$EXPORT_FILE_CONV.sql
+
 
---
+++ scripts/importDB-cod-V2toV3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Script d'importation de la base de données Intranet V2 vers V3
+#
+# Author: bs4300280 - 05/10/2015
+#
+#
+
+echo "* Importation dans l'environnement COD"
+./scripts/importDB-V2toV3.sh cod
+

b8f83d3e7df9a7d3c8b323d0dad2e18cfbd3f3f3
SalokineTerata <salokine@gmail.com>
2015-11-05 11:52:28 +0100

    [V] Script d'importation des données V2 en environnement Prod

---
+++ scripts/importDB-prd-V2toV3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Script d'importation de la base de données Intranet V2 vers V3
+#
+# Author: bs4300280 - 05/10/2015
+#
+#
+
+echo "* Importation dans l'environnement PRD"
+./scripts/importDB-V2toV3.sh prd
+

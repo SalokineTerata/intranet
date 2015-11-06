8368c082c8a11d8f9a573960538047717aab8b15
tp4300001 <tp4300001@pwn043126.avignon.grpldc.com>
2015-11-06 11:09:16 +0100

    test[V]Mise en fonction de la connexion PDO [V]Correction de l'implémentation PDO [Validation] ; [Correction de la validation] [Transition] [V]Réduction de tps d'execution de requête Sql [V]Mise en fonction de la gestion des droits d'utilisateur [V]Mise en forme de la nouvelle version de gestion des droits d'utilisateur [V]Modification de la liste d'espace de travail et de site [V]Mise en fonction de l'historique d'une Fta [V]Ajout d'index aux tables concernées [V]Ajout d'une clé étrangère aux tables concernées [V]Modification de la base de donnée [V]Mise en fonction du module extranet et de ces modifications [V]Modification du module admin agis [V]Mise en fonction du module planning de precence [V]Mise en fonction du module Fte [V]Correction de la mise en page de la page d'accueil [V]Correction de la liste de diffusion des utilisateurs [V]Modification de la mise en forme des tableau d'emballage [V]Génération des champs des chapitres de Qualité [V]Modification des des droits d'accès au bouton transition [V]Transition d'un workflow à un autre workflow [V]Mise en fonction du module acces [V]Pagination de la page d'accueil [V]Mise en fonction du module classification(Affichage) [V]Mise en fonction du workflow de présentation(a Voir) [V]Mise en fonction du module classification(Modification) [V]Mise en fonction du module classification(Suppression) [V]Mise en fonction du module classification(Ajout) [V]Changement de la classification sur le chapitre identité [V]Ajout des élements pré-saisies pendant la gestion d'une duplication Fta [V]Droits de consultation des Fta fonctionnel [V]Moteur de recherche (Fonctionnel V2 donnant un résultat V2) [V]Nouvelle mise en page [V]Nouveau script de migration [V]Mise en place de la page de chargement [V]Réécriture d'URL modification_fiche (uniquement pour le navigation, pas pour la redirection style : header) [V]Nouveau script de migration 2.0.1 [V]Amelioration deu tps d'execution de la page d'acccueil et de la déconnexion 3.0 [V]Modification de l'url page test historique
    []Mise en fonction de la génération de fichier PDF
    []Terminer la génération de logs

--- apps/fta/historique.php
+++ apps/fta/historique.php
@@ -58,7 +58,7 @@ $idFtaEtat = Lib::getParameterFromRequest(FtaEtatModel::KEYNAME);
 $abreviationFtaEtat = Lib::getParameterFromRequest(FtaEtatModel::FIELDNAME_ABREVIATION);
 $idFtaRole = Lib::getParameterFromRequest(FtaRoleModel::KEYNAME);
 $id_fta_chapitre = $id_fta_chapitre_encours;
-$ftaModel =new FtaModel($id_fta);
+$ftaModel = new FtaModel($id_fta);
 /* * ***********
   Début Code PHP
  * *********** */
@@ -84,7 +84,7 @@ $detail_id_fta;              //Identifiant de la fiche sur laquelle on souhaite 
 Navigation::initNavigation($id_fta, $id_fta_chapitre, $synthese_action, $comeback, $idFtaEtat, $abreviationFtaEtat, $idFtaRole);
 $navigue = Navigation::getHtmlNavigationBar();
 //Calcul du taux
-$taux_temp = FtaSuiviProjetModel::getFtaTauxValidation($ftaModel,TRUE);
+$taux_temp = FtaSuiviProjetModel::getFtaTauxValidation($ftaModel, TRUE);
 $details[$id_fta] = $taux_temp[1];
 
 //Détail d'un FTA
@@ -94,10 +94,13 @@ if ($id_fta) {
        <' . $html_table . '>
        <tr class=titre>
            <td>
+           ' . DatabaseDescription::getFieldDocLabel(FtaRoleModel::TABLENAME, FtaRoleModel::FIELDNAME_DESCRIPTION_FTA_ROLE) . '
+           </td>
+           <td>
            ' . DatabaseDescription::getFieldDocLabel('fta_processus', 'nom_fta_processus') . '
            </td>
            <td>
-           ' . DatabaseDescription::getFieldDocLabel('fta_processus', 'service_fta_processus') . '
+           ' . DatabaseDescription::getFieldDocLabel(FtaChapitreModel::TABLENAME, FtaChapitreModel::FIELDNAME_NOM_CHAPITRE) . '
            </td>
            <td>
            Taux
@@ -123,142 +126,54 @@ if ($id_fta) {
             //Chargement des données
 
             $ftaProcessusModel = new FtaProcessusModel($id_fta_processus);
-            if ($taux == 0) {
-                $idFtaProcessusEtat = 1;
-            } elseif ($taux <> 0 and $taux <> 1) {
-                $idFtaProcessusEtat = 2;
-            } elseif ($taux == 1) {
-                $idFtaProcessusEtat = 3;
+            if ($taux == "0") {
+                $idFtaProcessusEtat = "1";
+            } elseif ($taux <> "0" and $taux <> "1") {
+                $idFtaProcessusEtat = "2";
+            } elseif ($taux == "1") {
+                $idFtaProcessusEtat = "3";
             }
 
 
             $ftaProcessusEtatModel = new FtaProcessusEtatModel($idFtaProcessusEtat);
             $idSite = $ftaModel->getDataField(FtaModel::FIELDNAME_SITE_ASSEMBLAGE)->getFieldValue();
             $date_echeance_fta = $ftaModel->getDataField(FtaModel::FIELDNAME_DATE_ECHEANCE_FTA)->getFieldValue();
+            $idFtaWorkflow = $ftaModel->getDataField(FtaModel::FIELDNAME_WORKFLOW)->getFieldValue();
+            $arrayIdFtaChapitre = DatabaseOperation::convertSqlStatementWithoutKeyToArray(
+                            'SELECT DISTINCT ' . FtaChapitreModel::FIELDNAME_NOM_USUEL_CHAPITRE
+                            . ' FROM ' . FtaWorkflowStructureModel::TABLENAME . ',' . FtaChapitreModel::TABLENAME
+                            . ' WHERE ' . FtaWorkflowStructureModel::FIELDNAME_ID_FTA_WORKFLOW . '=' . $idFtaWorkflow
+                            . ' AND ' . FtaWorkflowStructureModel::FIELDNAME_ID_FTA_PROCESSUS . '=' . $id_fta_processus
+                            . ' AND ' . FtaChapitreModel::KEYNAME . '=' . FtaWorkflowStructureModel::FIELDNAME_ID_FTA_CHAPITRE
+            );
+            $nombreChapitre = count($arrayIdFtaChapitre);
+
+            foreach ($arrayIdFtaChapitre as $rowsIdFtaChapitre) {
+                $champChapitre = ' <td>
+                   &nbsp;' . $rowsIdFtaChapitre[FtaChapitreModel::FIELDNAME_NOM_USUEL_CHAPITRE] . ' ' . $nom_site . '
+                   </td> ';
+            }
             $geoModel = new GeoModel($idSite);
             $nom_site = $geoModel->getDataField(GeoModel::FIELDNAME_GEO)->getFieldValue();
             $nom_fta_processus = $ftaProcessusModel->getDataField(FtaProcessusModel::FIELDNAME_NOM)->getFieldValue();
             $service_fta_processus = $ftaProcessusModel->getDataField(FtaProcessusModel::FIELDNAME_SERVICE)->getFieldValue();
             $couleur_fta_processus_etat = $ftaProcessusEtatModel->getDataField(FtaProcessusEtatModel::FIELDNAME_COULEUR_PROCESSUS_ETAT)->getFieldValue();
             $nom_fta_processus_etat = $ftaProcessusEtatModel->getDataField(FtaProcessusEtatModel::FIELDNAME_NOM_PROCESSUS_ETAT)->getFieldValue();
-            /*
-              //Est-ce un processus multisite ?
-              $req = 'SELECT multisite_fta_processus FROM fta_processus '
-              . 'WHERE id_fta_processus='' . $id_fta_processus . '' '
-              ;
-              $result_temp = DatabaseOperation::query($req);
-              $multisite_fta_processus = mysql_result($result_temp, 0, 'multisite_fta_processus');
-              //Oui, il s'agit d'un Processus répartie sur les sites d'assemblage
-              if ($multisite_fta_processus) {
-              //Recherche du site en charge de ce processus
-              $req = 'SELECT geo FROM access_arti2, geo '
-              . 'WHERE id_fta=' . $id_fta . ' '
-              . 'AND Site_de_production=id_site '
-              ;
-              $result_temp = DatabaseOperation::query($req);
-              $geo = mysql_result($result_temp, 0, 'geo');
 
-              $nom_site = ' ($geo)';
-              } else {
-              $nom_site = '';
-              }
-
-              //Gestion des délais
-              $req = 'SELECT * FROM fta_processus_delai WHERE id_fta='' . $id_fta . '' AND id_fta_processus='' . $id_fta_processus . '' ';
-              $result = DatabaseOperation::query($req);
-              if (mysql_num_rows($result)) {
-              $date_echeance_processus = mysql_result($result, 0, 'date_echeance_processus');
-              $valide_fta_processus_delai = mysql_result($result, 0, 'valide_fta_processus_delai');
-              $couleur_fta_processus_delai = '';
-              $HTML_echeance = $date_echeance_processus;
-
-              $annee_date_echeance_processus = substr($date_echeance_processus, 0, 4);
-              $mois_date_echeance_processus = substr($date_echeance_processus, 5, 2);
-              $jour_date_echeance_processus = substr($date_echeance_processus, 8, 2);
-
-              //$jour_restant = mktime(0,0,0,$mois_date_echeance_processus - date('m'), $jour_date_echeance_processus - date('d'), $annee_date_echeance_processus - date ('Y'));
-              $echeance_timestamp = mktime(0, 0, 0, $mois_date_echeance_processus, $jour_date_echeance_processus, $annee_date_echeance_processus);
-              $today_timestamp = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
-              $seconde_restante = $echeance_timestamp - $today_timestamp;
-              //$jour_restant=$seconde_restante/(3600*24);
-              $jour_restant = round($seconde_restante / (3600 * 24), 0);
-
-              if ($valide_fta_processus_delai == 0) {
-
-
-              //echo $jour_restant.'<br>';
-              if ($jour_restant < 0) {
-              $couleur_fta_processus_delai = 'class=couleur_rouge';
-              $txt_echeance = 'échéance dépassée de';
-              $jour_restant = abs($jour_restant);
-              } else {
-              $couleur_fta_processus_delai = '';
-              $txt_echeance = 'reste';
-              }
-              $HTML_echeance.=' <i>($txt_echeance $jour_restant jours)</i>';
-              }
-              }//Fin du if(mysql_num_rows($result)
-              else {
-              $HTML_echeance = ' <i>Non défini</i>';
-              }
-
-              //Date de validation
-              //$req = 'SELECT * FROM fta_processus_delai WHERE id_fta=''.$id_fta.'' AND id_fta_processus=''.$id_fta_processus.'' ';
-              $req = 'SELECT MAX(date_validation_suivi_projet) AS date_validation_processus, id_fta_processus FROM fta_suivi_projet, fta_chapitre '
-              . 'WHERE id_fta='' . $id_fta . '' '
-              . 'AND signature_validation_suivi_projet <>0 '
-              . 'AND id_fta_processus='' . $id_fta_processus . '' '
-              . 'AND fta_suivi_projet.id_fta_chapitre=fta_chapitre.id_fta_chapitre '
-              . 'GROUP BY id_fta_processus'
-              ;
-              $result = DatabaseOperation::query($req);
-              if (mysql_num_rows($result)) {
-              $date_validation_processus = mysql_result($result, 0, 'date_validation_processus');
-              //$valide_id_fta_processus = mysql_result($result, 0, 'id_fta_processus');
-              //$couleur_fta_processus_delai = '';
-              $HTML_valide = $date_validation_processus;
-              if ($valide_fta_processus_delai <> 0) {
-              $annee_date_validation_processus = substr($date_validation_processus, 0, 4);
-              $mois_date_validation_processus = substr($date_validation_processus, 5, 2);
-              $jour_date_validation_processus = substr($date_validation_processus, 8, 2);
-
-              //$jour_restant = mktime(0,0,0,$mois_date_echeance_processus - date('m'), $jour_date_echeance_processus - date('d'), $annee_date_echeance_processus - date ('Y'));
-              $validation_timestamp = mktime(0, 0, 0, $mois_date_validation_processus, $jour_date_validation_processus, $annee_date_validation_processus);
-              //$today_timestamp = mktime(0,0,0,date('m'),date('d'),date('Y'));
-              $echeance_timestamp . '<br>'; //Cf. plus haut
-              $seconde_restante = $echeance_timestamp - $validation_timestamp;
-              //$jour_restant=$seconde_restante/(3600*24);
-              $jour_restant = round($seconde_restante / (3600 * 24), 0);
-
-              //echo $jour_restant.'<br>';
-              if ($jour_restant < 0) {
-              $couleur_fta_processus_valide = 'class=couleur_rouge';
-              $txt_valide = 'Validation dépassée de';
-              $jour_restant = abs($jour_restant);
-              } else {
-              $couleur_fta_processus_valide = '';
-              $txt_valide = '';
-              }
-              $HTML_valide.=' <i>($txt_valide $jour_restant jours)</i>';
-              }
-              }//Fin du if(mysql_num_rows($result)
-              else {
-              $couleur_fta_processus_valide = '';
-              $HTML_valide = ' <i>Non défini</i>';
-              } */
             //Ecriture du code HTML
             $bloc.= '
-               <tr class=contenu>
-                   <td>
-                   &nbsp;' . $nom_fta_processus . ' ' . $nom_site . '
-                   </td>
-                   <td>
+               <tr class=contenu >
+                    <td ROWSPAN=' . $nombreChapitre . ' >
                    &nbsp;' . $service_fta_processus . '
                    </td>
-                   <td>
-                   &nbsp;' . round($taux * 100, 2) . '%
+                   <td ROWSPAN=' . $nombreChapitre . '>
+                   &nbsp;' . $nom_fta_processus . ' ' . $nom_site . '
+                   </td>   
+                   
+                   <td ROWSPAN=' . $nombreChapitre . '>
+                   &nbsp;' . round($taux * "100", "2") . '%
                    </td>
-                   <td bgcolor=' . $couleur_fta_processus_etat . '>
+                   <td bgcolor=' . $couleur_fta_processus_etat . ' ROWSPAN=' . $nombreChapitre . '>
                    &nbsp;' . $nom_fta_processus_etat . '
                    </td>
                   

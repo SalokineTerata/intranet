131532832242ce558be39f77109e6c08d4ce508c
tp4300001 <tp4300001@pwn043126.avignon.grpldc.com>
2016-01-07 15:34:08 +0100

    [V] Désactivation du contôle de la date d'échéance [ENCOURS] Modification de l'espace de travail CoupeFe

--- apps/upgrade/upgrade_CoupeFe.php
+++ apps/upgrade/upgrade_CoupeFe.php
@@ -1,19 +1,38 @@
 <?php
 
-/* 
- * Copyright (C) 2016 tp4300001
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+require_once '../inc/php.php';
 
+$arrayClassifIncomplete = DatabaseOperation::convertSqlStatementWithoutKeyToArray(
+                "SELECT " . FtaModel::KEYNAME . "," . FtaModel::FIELDNAME_ID_FTA_ETAT
+                . " FROM " . FtaModel::TABLENAME
+                . " WHERE " . FtaModel::FIELDNAME_WORKFLOW . "=" . "6"
+);
+if ($arrayClassifIncomplete) {
+    foreach ($arrayClassifIncomplete as $rowsClassifInComplete) {
+        $idFta = $rowsClassifInComplete[FtaModel::KEYNAME];
+        $IdFtaEtat = $rowsClassifInComplete[FtaModel::FIELDNAME_ID_FTA_ETAT];
+
+        /**
+         * Initailisation du nouveau chapitre
+         */
+        FtaSuiviProjetModel::initFtaSuiviProjet($idFta);
+
+        if ($IdFtaEtat <> FtaEtatModel::ID_VALUE_MODIFICATION) {
+            $validation = DatabaseOperation::execute(
+                            "UPDATE " . FtaSuiviProjetModel::TABLENAME
+                            . " SET " . FtaSuiviProjetModel::FIELDNAME_SIGNATURE_VALIDATION_SUIVI_PROJET . "=" . FtaSuiviProjetModel::SIGNATURE_VALIDATION_SUIVI_PROJET_AUTO
+                            . " WHERE " . FtaSuiviProjetModel::FIELDNAME_ID_FTA . "=" . $idFta
+                            . " AND " . FtaSuiviProjetModel::FIELDNAME_ID_FTA_CHAPITRE . "=" . "41"
+            );
+            if ($validation) {
+                echo FtaSuiviProjetModel::FIELDNAME_ID_FTA . "=" . $idFta . " OK <br>";
+            } else {
+                echo FtaSuiviProjetModel::FIELDNAME_ID_FTA . "=" . $idFta . " FAILDED <br>";
+            }
+        } else {
+            echo "Cette Id " . $idFta . " Fta n'est pas à validé<br>";
+        }
+    }
+} else {
+    echo "<br>Vous vennez d'executer un script interdit <br> CONTACTEZ IMMEDIATEMENT L'ADMINISTRATEUR DU SITE!!!!!!!!!!!!!<br>";
+}

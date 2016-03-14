f478dded27cdbadf6ee5e02ae9e53759c15e66a0
tp4300001 <tp4300001@pwn043126.avignon.grpldc.com>
2016-03-14 17:13:09 +0100

    [V] Page de chargement sur mozzila [V] Ordonnance page d'accueil

--- apps/lib/functions.html.php
+++ apps/lib/functions.html.php
@@ -630,7 +630,7 @@ function print_page_begin($disable_full_page = FALSE, $menu_file = NULL, $conf =
     echo "<body $printable leftmargin=0 topmargin=0 marginwidth=0 marginheight=0 onload=chargement();>";
 
     echo "<div class=display_none id=chargement style=width:100%;height:75px;color:red;font-weight:bold;font-size:14px;background:white;>
-           <img src= ..\lib\images\ajax_loader.gif> Chargement ...
+           <img src= ../lib/images/ajax_loader.gif> Chargement ...
           </div>";
 
 

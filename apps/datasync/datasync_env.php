550b5d419ea8804ad0d30c487d514c7a959c33ff
tp4300001 <tp4300001@pwn043126.avignon.grpldc.com>
2016-03-24 11:33:43 +0100

    [ENCOURS] VIDAGE des caches lors de la navigation [ENCOURS] Ajout du module dataSync

--- apps/datasync/datasync_env.php
+++ apps/datasync/datasync_env.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * Identification du type de sycnhronication
  * -d Copy de la journée 
@@ -12,21 +11,13 @@ $type = $argv[1];
  * Détermination de l'environnement
  */
 $serverName = file_get_contents("/etc/hosts");
-
-switch ($serverName) {
-    case "cod-intranet.agis.fr":
-        echo exec('./cli/datasync_cod.sh ' . $type);
-
-        break;
-    case "dev-intranet.agis.fr":
-
-        echo exec('./cli/datasync_dev.sh ' . $type);
-        break;
-    case "fta05401.grpldc.com":
-
-        echo exec('./cli/datasync_prd.sh ' . $type);
-        break;
-    
-    default :
-        echo $serverName;
+if (stripos($serverName, "cod-intranet.agis.fr")) {
+    echo exec('./cli/datasync_cod.sh ' . $type);
+} elseif (stripos($serverName, "dev-intranet.agis.fr")) {
+    echo exec('./cli/datasync_dev.sh ' . $type);
+} elseif (stripos($serverName, "fta05401.grpldc.com")) {
+    echo exec('./cli/datasync_prd.sh ' . $type);
+} else {
+    echo $serverName;
 }
+

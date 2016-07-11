f7b88c113a9c60cf7d25424b15b239c68625f1a9
fa4301632 <fa4301632@pwn043126.avignon.grpldc.com>
2016-07-11 11:05:16 +0200

    [Encours] Fta2Arcadia test de Gestion du retour du fichier9.0

--- apps/upgrade/upgrade2_Fta2Arcadia_data.php
+++ apps/upgrade/upgrade2_Fta2Arcadia_data.php
@@ -34,65 +34,42 @@ echo "NBfolder = " . count($folder);
 for ($i = 0; $i < count($folder); $i++) {
     $file = $folder[$i];
     echo "file = " . $file;
-    $fileContent=file_get_contents($linkFolder . $file);
+    $fileContent = file_get_contents($linkFolder . $file);
     $xml = XMLReader::open($linkFolder . $file);
     $xml->setParserProperty(XMLReader::VALIDATE, true);
     $valide = $xml->isValid();
     if ($valide) {
-        $parametres = simplexml_load_file($linkFolder . $file);
-        if (is_object($parametres)) {
-            $dom = new DomDocument;
-            $dom->load($linkFolder . $file);
-            $Transaction = $dom->getElementsByTagName("Transaction");
-            if ($Transaction) {
-                foreach ($Transaction as $value) {
-                    $idTransaction = $value->getAttribute("id");
-                }
-            }
-            $idFta = $parametres->xpath("IdFta");
-            $codeReply = $parametres->xpath("CodeReply");
-            $codeArticleArcadia = $parametres->xpath("IdArcadia");
+        $parametres = simplexml_load_file($fileContent);
+        $dom = new DomDocument;
+        $dom->load($linkFolder . $file);
+        $Transaction = $dom->getElementsByTagName("Transaction");
 
-            $sql_inter = "UPDATE " . $nameOfBDDTarget . "." . "fta2arcadia_transaction"
-                    . " SET " . "code_reply" . "=" . $codeReply
-                    . ", " . "code_article_ldc" . "=" . $codeArticleArcadia
-                    . " WHERE " . 'id_fta' . "=" . $idFta
-                    . " AND " . 'id_arcadia_transaction' . "=" . $idTransaction;
-            echo "UPDATE " . $nameOfBDDTarget . "." . "fta2arcadia_transaction"
-            . " SET " . "code_reply" . "=" . $codeReply
-            . ", " . "code_article_ldc" . "=" . $codeArticleArcadia
-            . " WHERE " . 'id_fta' . "=" . $idFta
-            . " AND " . 'id_arcadia_transaction' . "=" . $idTransaction . " ...";
-            if (mysql_query($sql_inter)) {
-                echo "[OK]\n";
-            } else {
-                echo "[FAILED]\n";
-            }
+        foreach ($Transaction as $value) {
+            $idTransaction = $value->getAttribute("id");
+        }
+
+        $idFta = $parametres->xpath("IdFta");
+        $codeReply = $parametres->xpath("CodeReply");
+        $codeArticleArcadia = $parametres->xpath("IdArcadia");
+
+        $sql_inter = "UPDATE " . $nameOfBDDTarget . "." . "fta2arcadia_transaction"
+                . " SET " . "code_reply" . "=" . $codeReply
+                . ", " . "code_article_ldc" . "=" . $codeArticleArcadia
+                . " WHERE " . 'id_fta' . "=" . $idFta
+                . " AND " . 'id_arcadia_transaction' . "=" . $idTransaction;
+        echo "UPDATE " . $nameOfBDDTarget . "." . "fta2arcadia_transaction"
+        . " SET " . "code_reply" . "=" . $codeReply
+        . ", " . "code_article_ldc" . "=" . $codeArticleArcadia
+        . " WHERE " . 'id_fta' . "=" . $idFta
+        . " AND " . 'id_arcadia_transaction' . "=" . $idTransaction . " ...";
+        if (mysql_query($sql_inter)) {
+            echo "[OK]\n";
+        } else {
+            echo "[FAILED]\n";
         }
     }
 }
 
-while ($rowsFta = mysql_fetch_array($arrayFta)) {
-    $arrayIdFtaClassfication = mysql_query(
-            "SELECT DISTINCT id_fta_classification2 "
-            . " FROM " . $nameOfBDDTarget . ".classification_fta, " . $nameOfBDDTarget . ".classification_fta2"
-            . " WHERE " . $nameOfBDDTarget . ".classification_fta.id_classification_arborescence_article = " . $nameOfBDDTarget . ".classification_fta2.id_arborescence"
-            . " AND " . $nameOfBDDTarget . ".classification_fta.id_fta = " . $rowsFta['id_fta']
-    );
-    if ($arrayIdFtaClassfication) {
-        while ($value = mysql_fetch_array($arrayIdFtaClassfication)) {
-            $sql_inter = "UPDATE " . $nameOfBDDTarget . "." . "fta"
-                    . " SET " . "id_fta_classification2" . "=" . $value["id_fta_classification2"]
-                    . " WHERE " . 'id_fta' . "=" . $rowsFta['id_fta'];
-            echo "UPDATE " . $nameOfBDDTarget . "." . "fta." . 'id_fta' . "=" . $rowsFta['id_fta'] . " id_fta_classification2" . "=" . $value["id_fta_classification2"] . " ...";
-            if (mysql_query($sql_inter)) {
-                echo "[OK]\n";
-            } else {
-                echo "[FAILED]\n";
-            }
-        }
-    }
-}
 
 $debut = date("H:i:s");
 
